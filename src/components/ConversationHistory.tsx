6b5eed3ba895d10d0db500d7c04a7a582fb68744
Mohamed Barakat <58600255+MLBarakat@users.noreply.github.com>
2025-09-17 18:42:45 -0500

    changes

--- src/components/ConversationHistory.tsx
+++ src/components/ConversationHistory.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Message } from '../types/conversation';
 import { DisplayUser } from '../types/user';
 import ImmigoLogo from '../assets/immigo_logo.png';
