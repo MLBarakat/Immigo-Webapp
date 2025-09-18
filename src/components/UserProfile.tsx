072148c06dd7f93de106db615b5573c4bc009007
Mohamed Barakat <58600255+MLBarakat@users.noreply.github.com>
2025-09-17 23:05:53 -0500

    Update UserProfile.tsx

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -9,3 +9,13 @@ export interface UserProfile {
     full_name: string;
     language: string;
 }
+
+export function UserBubble({ user }: UserProfileProps): JSX.Element {
+  return (
+    <div className="flex items-center space-x-2">
+      <div className="flex items-center justify-center w-10 h-10 rounded-full bg-art-blue-100 text-art-blue-700 font-bold text-lg flex-shrink-0">
+        {user.initials}
+      </div>
+    </div>
+  );
+}
