52bcde528788071b7725dbdd700747941a309511
Mohamed Barakat <58600255+MLBarakat@users.noreply.github.com>
2025-09-20 23:54:53 -0500

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="w-full bg-star-white dark:bg-gray-800 text-immigo-gray-600 dark:text-immigo-gray-400 text-center py-3 text-sm border-t-2 border-deep-navy dark:border-gray-700 mt-auto">
+    <footer className="w-full bg-star-white dark:bg-gray-800 text-immigo-gray-600 dark:text-immigo-gray-400 text-center py-3 text-sm border-t-2 border-immigo-gray-300 dark:border-gray-700 mt-auto">
       &copy; {currentYear} ImmiGO. All rights reserved.
     </footer>
   );
