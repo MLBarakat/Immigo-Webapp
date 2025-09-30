64e83843d93ece3e0084369802cd3909134f70c2
Mohamed Barakat <58600255+MLBarakat@users.noreply.github.com>
2025-09-29 19:41:24 -0500

    Update resource.ts

--- amplify/functions/resource.ts
+++ amplify/functions/resource.ts
@@ -1,8 +1,6 @@
 import { defineFunction } from '@aws-amplify/backend';
 
 export const myApiFunction = defineFunction ({
-    entry: './handler.ts',
     memoryMB: 512,
     timeoutSeconds: 30,
-    exportName: 'handler',
 });
